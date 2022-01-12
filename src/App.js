4dd962c92cfadd0006cffc8b10d2d42fdf7f2abb
Jesus-Montesinos <fredturnerr31@gmail.com>
2022-01-11 21:32:48 -0600

    button click event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [pokemonName, setpokemonName] = useState([]);
+  const [descriptionPokemon, setdescriptionPokemon] = useState([]);
+
+  useEffect(() => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const [item] = data.types;
+        const { type } = item;
+        setdescriptionPokemon(type);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, [pokemonName]);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setpokemonName(e.target.value);
+    }
+  };
+
+  const handleKeyEnter = (e) => alert("testing onclick event with ReactBaby!!")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Puchamon</h1>
+      <input type="text" onKeyPress={handleKeyPress}></input>
+      <button type="button" onClick={handleKeyEnter}>Enter Please</button>
+      <h2>Start editing to see some magic happen!</h2>
+      <h1>{pokemonName}</h1>
+      <h5>Type</h5>
+      <h5>{descriptionPokemon.name}</h5>
     </div>
   );
 }
-
-export default App;
